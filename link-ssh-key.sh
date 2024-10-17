0596eae0d064a5fb1b24eac9a7e84f126777928b
Sebastien Boisvert <sebhtml@protonmail.com>
2024-10-17 12:57:23 +0000

    tools: fix ssh key script

--- link-ssh-key.sh
+++ link-ssh-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ssh-keygen -t ed25519
+cp /workspace/id_ed25519.pub ~/.ssh/id_ed25519.pub
+cp /workspace/id_ed25519 ~/.ssh/id_ed25519
 
-ln -s /workspace/id_ed25519 ~/.ssh/id_ed25519
-ln -s /workspace/id_ed25519.pub ~/.ssh/id_ed25519.pub
-
+chmod 0600 ~/.ssh/id_ed25519.pub
+chmod 0600 ~/.ssh/id_ed25519
