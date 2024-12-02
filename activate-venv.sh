af87cc0949f2c31b71320e9702ab5535c6a0d3f1
Sebastien Boisvert <sebhtml@protonmail.com>
2024-12-02 21:54:51 +0000

    tools: add script to activate venv

---
+++ activate-venv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#VENV_NAME="venv/runpod/pytorch-2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04/venv"
+VENV_NAME="venv/runpod/pytorch-2.4.0-py3.11-cuda12.4.1-devel-ubuntu22.04/venv"
+
+source ${VENV_NAME}/bin/activate
