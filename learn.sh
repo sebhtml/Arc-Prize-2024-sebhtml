9892dbe48367e69c9512e2ba158db6a50e96da3b
Sebastien Boisvert <sebhtml@protonmail.com>
2024-12-02 21:55:07 +0000

    chore: use correct activation script

--- learn.sh
+++ learn.sh
@@ -2,7 +2,7 @@
 
 cd /workspace/Arc-Prize-2024-sebhtml
 
-source venv/bin/activate
+source activate-venv.sh
 
 log_file=$(date --iso-8601=seconds).log
 (time python src/main.py) &> /workspace/logs/$log_file
