24b58330291723eb20c14d2973165e0290b3013d
Sebastien Boisvert <sebhtml@protonmail.com>
2025-01-02 14:09:56 +0000

    tools: install cloc

--- install-programs.sh
+++ install-programs.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
+
 apt-get update -y
-apt-get install -y vim less tmux git linux-tools-common hdf5-tools jq
-
-
+apt-get install -y vim less tmux git linux-tools-common hdf5-tools jq cloc
